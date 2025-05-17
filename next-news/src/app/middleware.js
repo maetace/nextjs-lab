8e6c6feda52052b16d98e2fde08098198556c707
maetace <itpirain@gmail.com>
2025-05-17 23:53:47 +0700

    Lab 6 > Render.com fixed #2

--- next-news/src/app/middleware.js
+++ next-news/src/app/middleware.js
@@ -4,19 +4,6 @@ export const config = {
     matcher: ['/news/:path*', '/api/:path*'],
 };
 
-// export function middleware(request) {
-//     console.log('Incoming request:', request.method, request.url);
-
-//     // ตัวอย่าง redirect (กรณีผู้ใช้ไม่ได้ login)
-//     // if (!request.cookies.get('auth')) {
-//     //   return NextResponse.redirect(new URL('/login', request.url));
-//     // }
-
-//     return NextResponse.next(); // อนุญาตให้ผ่านไปยัง route ปลายทาง
-// }
-
-import { NextResponse } from 'next/server';
-
 export function middleware(request) {
     const { pathname } = request.nextUrl;
 
