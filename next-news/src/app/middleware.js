1007e9f3d9268095491e4e0da9cafe0ebcf8c6a0
maetace <itpirain@gmail.com>
2025-05-24 19:01:03 +0700

    Lab 8 Finished

--- next-news/src/app/middleware.js
+++ next-news/src/app/middleware.js
@@ -1,18 +1,23 @@
 import { NextResponse } from 'next/server';
 
 export const config = {
-    matcher: ['/news/:path*', '/api/:path*'],
+    matcher: ['/news/:path*', '/api/:path*', '/admin/:path*'],
 };
 
 export function middleware(request) {
     const { pathname } = request.nextUrl;
+    const authCookie = request.cookies.get('auth');
 
-    // บล็อกการลบข่าว ถ้าไม่ได้ส่ง header x-admin=true
     if (pathname.startsWith('/api/news') && request.method === 'DELETE') {
         const isAdmin = request.headers.get('x-admin') === 'true';
         if (!isAdmin) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
+    }
+
+    if (!pathname.startsWith('/admin/auth') && pathname.startsWith('/admin') && !authCookie) {
+        const loginUrl = new URL('/admin/auth', request.url);
+        return NextResponse.redirect(loginUrl);
     }
 
     return NextResponse.next();
