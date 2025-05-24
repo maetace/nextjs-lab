b8441e950c077e83783c1b45adf75e94d1b2ece5
maetace <itpirain@gmail.com>
2025-05-24 18:37:08 +0700

    Lab 7

--- next-news/next.config.mjs
+++ next-news/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// const nextConfig = {};
+
+// export default nextConfig;
+
+const nextConfig = {
+    images: {
+        domains: ['localhost'],
+    },
+};
 
 export default nextConfig;
--- next-news/src/app/api/news/route.js
+++ next-news/src/app/api/news/route.js
@@ -1,25 +1,28 @@
-import { getAllNews } from '@/lib/news';
+import { getAllNews, addNews } from '@/lib/news';
 
-export function GET() {
-  return Response.json(getAllNews());
+export async function GET() {
+  try {
+    const news = await getAllNews();
+    return Response.json(news);
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
 }
 
-export async function POST(request) {
-  const body = await request.json();
+export async function POST(req) {
+  try {
+    const form = await req.formData();
+    const news = {
+      slug: form.get('slug'),
+      title: form.get('title'),
+      content: form.get('content'),
+      date: form.get('date'),
+    };
+    const file = form.get('image');
 
-  if (!body.title || !body.content || !body.slug) {
-    return Response.json({ error: 'Missing fields' }, { status: 400 });
+    const result = await addNews(news, file);
+    return Response.json(result, { status: 201 });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
   }
-
-  const newItem = {
-    id: 'n' + Date.now(),
-    title: body.title,
-    slug: body.slug,
-    content: body.content,
-    image: body.image || 'news.jpg',
-    date: new Date().toISOString().split('T')[0],
-  };
-
-  addNews(newItem);
-  return Response.json({ message: 'News added', data: newItem }, { status: 201 });
 }
